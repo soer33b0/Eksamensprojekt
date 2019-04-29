86a2322fa71c74b4f9f7f242b09ac1d8ccf98186
Søren Gjedsøe Eriksen <soer33b0@edu.eal.dk>
2019-04-29 14:21:05 +0200

    Invoice properties og constructor

--- Domain/Invoice.cs
+++ Domain/Invoice.cs
@@ -8,9 +8,45 @@ namespace Domain
 {
     public class Invoice
     {
-        public Invoice(Customer customer, Employee employee, string date, int hoursWorked, double hourlySalary, double moms, double totalSalary)
-        {
+        Employee employee = new Employee("");
 
+        public string Date
+        {
+            get;
+            set;
+        }
+
+        public double HoursWorked
+        {
+            get;
+            set;
+        }
+
+        public double HourlySalary
+        {
+            get;
+            set;
+        }
+
+        public double VAT //moms
+        {
+            get;
+            set;
+        }
+
+        public double TotalSalary
+        {
+            get;
+            set;
+        }
+
+        public Invoice(string _date, int _hoursWorked, double _hourlySalary, double _VAT, double _totalSalary)
+        {
+            Date = _date;
+            HoursWorked = _hoursWorked;
+            HourlySalary = _hourlySalary;
+            VAT = _VAT;
+            TotalSalary = _totalSalary;
         }
     }
 }
