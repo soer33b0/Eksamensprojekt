3ae7f8bd9e3117e3866a2c1967333405e03fece0
Søren Gjedsøe Eriksen <soer33b0@edu.eal.dk>
2019-04-30 13:33:29 +0200

    SaveInvoice lavet i DBcontroller

--- ApplicationLayer/DBController.cs
+++ ApplicationLayer/DBController.cs
@@ -15,18 +15,22 @@ namespace ApplicationLayer
         Invoice invoice = new Invoice("29-04-2019", 31, 100, 3100);
         private string connectionString = "Server = ealSQL1.eal.local; Database = A_DB30_2018 ; User Id = A_STUDENT30; Password=A_OPENDB30;";
 
-        public void SaveInvoice()
+        public void SaveInvoice(DateTime date, double hoursWorked, double hourlySalary, double totalSalary)
         {
             using (SqlConnection conn = new SqlConnection())
             {
                 conn.Open();
                 try
                 {
-                    SqlConnection connection = new SqlConnection(connectionString);
-                    connection.Open();
-
                     SqlCommand saveInvoice = new SqlCommand("SaveInvoice", conn);
                     saveInvoice.CommandType = CommandType.StoredProcedure;
+                    saveInvoice.Parameters.Add(new SqlParameter("@InvoiceDate", date));
+                    saveInvoice.Parameters.Add(new SqlParameter("@HoursWorked", hoursWorked));
+                    saveInvoice.Parameters.Add(new SqlParameter("@HourlySalary", hourlySalary));
+                    saveInvoice.Parameters.Add(new SqlParameter("@TotalSalary", totalSalary));
+                    saveInvoice.ExecuteNonQuery();
+                    Console.Clear();
+                    Console.WriteLine("Faktura gemt!");
                 }
 
                 catch (SqlException e)
