2cdef787fc393fe59e4a6e9ed146a119e1dbe770
Søren Gjedsøe Eriksen <soer33b0@edu.eal.dk>
2019-05-02 14:34:28 +0200

    Generere faktura halvt færdig

--- DomainLayer/InvoiceGen.cs
+++ DomainLayer/InvoiceGen.cs
@@ -5,27 +5,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xceed.Words.NET;
+using DomainLayer;
 
 namespace DomainLayer
 {
     public class InvoiceGen
     {
-        public void OpenDocx()
+        public void OpenDocx(string dir, string fileName)
         {
-            string fileName = @"C:\Users\Søren\Desktop\Eksamensprojekt\Fakturaskabelon.docx";
-            var doc = DocX.Load(fileName);
-
-            doc.SaveAs("temp.docx");
+            string lort = dir + fileName;
+            var doc = DocX.Load(lort);
 
             //Process.Start("WINWORD.EXE", @"C:\Users\Søren\source\repos\Eksamensprojekt\UI\bin\Debug\temp.docx");
         }
 
-        public void ReplaceInvoiceText(string customerName, string customerAddress, string customerZipCity, string employeeName, string employeeAddress, string employeeZipCity, string employeeSeNum, string employeeAccountNum, DateTime invoiceDate, string invoiceNum, string title, double VAT, double totalSalary)
+        public void SaveDocx(DocX doc, string dir, string fileName)
         {
-            string fileName = @"";
+            doc.SaveAs(dir + fileName);
+        }
+
+        public void ReplaceInvoiceText(Customer customer, Employee employee, Invoice invoice)
+        {
+            string fileName = @"C:\Users\Søren\source\repos\Eksamensprojekt\UI\bin\Debug\temp.docx";
             var doc = DocX.Load(fileName);
 
+            doc.ReplaceText("%customerName%", customer.CustomerName);
+            doc.ReplaceText("%customerAddress%", customer.CustomerAddress);
+            doc.ReplaceText("%customerZipCity%", customer.CustomerZipCity);
 
+            doc.ReplaceText("%employeeName%", employee.EmployeeName);
+            doc.ReplaceText("%employeeAddress%", employee.EmployeeAddress);
+            doc.ReplaceText("%employeeZipCity%", employee.EmployeeZipCity);
+            doc.ReplaceText("%seNum%", employee.EmployeeSeNum);
+            doc.ReplaceText("%employeeAccountNum%", employee.EmployeeAccountNum);
+
+            doc.ReplaceText("%invoiceDate%", invoice.Date);
+
+            doc.SaveAs(@"C:\Users\Søren\source\repos\Eksamensprojekt\UI\bin\Debug\temp.docx");
         }
         
 
--- UI/CreateInvoice2.xaml.cs
+++ UI/CreateInvoice2.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -31,15 +32,22 @@ namespace UI
             LineItemCount++;
             ItemCount.Content = LineItemCount;
 
-            //metodekald indsæt i tabel
-            InvoiceGen invoiceGen = new InvoiceGen();
-            invoiceGen.OpenDocx();
+            //metodekald indsæt kolonne
 
             //Description.Text = "";
             //NumOfHours.Text = "";
             //HourlySalary.Text = "";
             //MessageBox.Show("Punkt tilføjet!");
-            
+
+            InvoiceGen invoiceGen = new InvoiceGen();
+            invoiceGen.OpenDocx(@"C:\Users\Søren\Desktop\Eksamensprojekt", @"\Fakturaskabelon.docx");
+
+            Customer cust = new Customer("Hans", "hula bulla gade", "123@nej.dk", "33313215", "4412");
+            Employee emp = new Employee("Jørn Jensen", "nejgade 42", "4000 odense Q", "12355578876", "6352 6125343124");
+            Invoice inv = new Invoice("21-03-2018", 420, 220, 412534);
+
+            invoiceGen.ReplaceInvoiceText(cust, emp, inv);
+
         }
     }
 }
