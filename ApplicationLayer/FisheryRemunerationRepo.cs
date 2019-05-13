015336a260a9ee099ec6f50428ec8f941822eff3
rune7536 <rune7536edu.eal.dk>
2019-05-13 11:23:07 +0200

    Invoice, fisheryremuneration repos

---
+++ ApplicationLayer/FisheryRemunerationRepo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainLayer;
+
+namespace ApplicationLayer
+{
+    public class FisheryRemunerationRepo
+    {
+        private List<FisheryRemuneration> ListFisheryRemuneration;
+
+        public FisheryRemunerationRepo()
+        {
+            ListFisheryRemuneration = new List<FisheryRemuneration>();
+        }
+
+        public void AddFisheryRemuneration(FisheryRemuneration fisheryRemuneration)
+        {
+            ListFisheryRemuneration.Add(fisheryRemuneration);
+        }
+
+        public List<FisheryRemuneration> GetFisheryRemuneration()
+        {
+            List<FisheryRemuneration> showFisheries = new List<FisheryRemuneration>();
+            return showFisheries;
+        }
+    }
+}
---
+++ ApplicationLayer/InvoiceRepo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainLayer;
+
+namespace ApplicationLayer
+{
+    public class InvoiceRepo
+    {
+        private List<Invoice> ListInvoice;
+
+        public InvoiceRepo()
+        {
+            ListInvoice = new List<Invoice>();
+        }
+
+        public void AddInvoice(Invoice invoice)
+        {
+            ListInvoice.Add(invoice);
+        }
+
+        public List<Invoice> GetInvoice()
+        {
+            List<Invoice> showInvoice = new List<Invoice>();
+            return showInvoice;
+        }
+    }
+}
