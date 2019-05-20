223cb012374649831f5b778c56aca256cf23fc17
Søren Gjedsøe Eriksen <soer33b0@edu.eal.dk>
2019-05-20 12:58:57 +0200

    createcustomer oprettet

---
+++ UI/CreateCustomer.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ApplicationLayer;
+using DomainLayer;
+
+namespace UI
+{
+    /// <summary>
+    /// Interaction logic for CreateCustomer.xaml
+    /// </summary>
+    public partial class CreateCustomer : Page
+    {
+        public CreateCustomer()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Controller controller = new Controller();
+            if (string.IsNullOrWhiteSpace(customerName.Text) || string.IsNullOrWhiteSpace(customerAddress.Text) || string.IsNullOrWhiteSpace(customerZipCity.Text) || string.IsNullOrWhiteSpace(customerEmail.Text) || string.IsNullOrWhiteSpace(customerPhone.Text))
+            {
+                MessageBox.Show("fuck niggers");
+            }
+            else
+            {
+                controller.AddCustomer(customerName.Text, customerAddress.Text, customerZipCity.Text, customerEmail.Text, customerPhone.Text);
+            }
+            
+        }
+    }
+}
---
+++ UI/Management.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UI
+{
+    /// <summary>
+    /// Interaction logic for Management.xaml
+    /// </summary>
+    public partial class Management : Window
+    {
+        public Management()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Customer(object sender, RoutedEventArgs e)
+        {
+            CreateCustomer createCustomer = new CreateCustomer();
+            this.Content = createCustomer;
+        }
+    }
+}
