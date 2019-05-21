1ea2f787bd2e8ebe0183872436387cdde1eb8546
Søren Gjedsøe Eriksen <soer33b0@edu.eal.dk>
2019-05-21 12:17:55 +0200

    ret

--- ApplicationLayer/DBController.cs
+++ ApplicationLayer/DBController.cs
@@ -159,7 +159,7 @@ namespace ApplicationLayer
                     {
                         while (read.Read())
                         {
-                            templist.Add(read.GetString(1));
+                            templist.Add(read.GetString(0));
                         }
                     }
                 }
