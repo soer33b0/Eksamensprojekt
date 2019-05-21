c995d8562a758603b4ec4b66c720124d334ce14d
Søren Gjedsøe Eriksen <soer33b0@edu.eal.dk>
2019-05-21 15:27:02 +0200

    3

--- UI/CreateInvoice.xaml.cs
+++ UI/CreateInvoice.xaml.cs
@@ -58,7 +58,7 @@ namespace UI
             customer = customerRepo.GetCustomerAtIndex(selectedIndex, selectedItem);
             if (customer == null)
             {
-                MessageBox.Show("fuck niggers");
+                MessageBox.Show("Fejl! 404 kunde not found");
             }
 
             invoice.InvoiceTitle = Title.Text;
